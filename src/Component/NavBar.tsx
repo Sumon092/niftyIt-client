2e309f1e8bfc8a986245116a40aa0a7485ac65f1
Sumon <m.sumon@outlook.com>
2023-01-13 00:17:48 +0600

    navebar and login complete

---
+++ src/Component/NavBar.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+    return (
+        <>
+            <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-teal-900">
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <Link to=""
+                            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+
+                        >
+                            NitfyIt
+                        </Link>
+                        <button
+                            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                            <i className="fas fa-bars"></i>
+                        </button>
+                    </div>
+                    <div
+                        className={
+                            "lg:flex flex-grow items-center" +
+                            (navbarOpen ? " flex" : " hidden")
+                        }
+                        id="example-navbar-danger"
+                    >
+                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                            <li className="nav-item">
+                                <Link to="/profile"
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+
+                                >
+                                    <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Profile</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/product"
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+
+                                >
+                                    <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Product</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/login"
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+
+                                >
+                                    <span>Login</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/signup"
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+
+                                >
+                                    <span>signup</span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+
+const Signup = () => {
+    return (
+        <div className="bg-gray-900 h-auto w-screen relative overflow-hidden flex justify-center items-center p-16">
+            <div className="h-40-r w-40-r bg-gradient-to-r from-green-400 to-white-500 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
+            <div className="h-35-r w-35-r bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-full absolute top-96 -left-20 transform rotate-180 animate-pulse"></div>
+            <Tilt>
+                <div className="container h-96 w-96 bg-white bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
+                    <form className='h-full flex flex-col justify-evenly items-center'>
+                        <div className='text-white font-poppins text-2xl tracking-widest'>Signup</div>
+                        <input type="text" placeholder='Enter Your Name' className="font-poppins bg-transparent border border-t-0 border-l-0 border-r-0 focus:outline-none text-white tracking-wide" />
+                        <input type="email" placeholder='Enter Your Email' className="font-poppins bg-transparent border border-t-0 border-l-0 border-r-0 focus:outline-none text-white tracking-wide" />
+                        <input type="password" placeholder='password' className="font-poppins bg-transparent border border-t-0 border-l-0 border-r-0 focus:outline-none text-white tracking-wide" />
+                        <input type="password" placeholder='Confirm Password' className="font-poppins bg-transparent border border-t-0 border-l-0 border-r-0 focus:outline-none text-white tracking-wide" />
+
+                        <input type="Submit" className='cursor-pointer font-poppins rounded-full px-5 py-2 bg-white bg-opacity-50 hover:bg-white hover:bg-opacity-80 ' />
+                    </form>
+                </div>
+            </Tilt>
+        </div>
+    );
+};
+
+export default Signup;
